93463c87035be7d9e1287fa14f7a5385f9b8c5a7
Nic0lasDev <94779829+Nic0lasDev@users.noreply.github.com>
2022-12-16 17:29:12 +0100

    basic js ready

---
+++ index.js
@@ -0,0 +1,26 @@
+// je récupère tous les inputs
+const nameInput = document.querySelector('#name');
+const cardNumberInput = document.querySelector('#number');
+const monthInput = document.querySelector('.month');
+const yearInput = document.querySelector('.year');
+const cvcInput = document.querySelector('#cvc');
+// const button = document.querySelector('.button');
+const form = document.querySelector('#form');
+
+// je récupères les eléméents des cartes
+const nameEl = document.querySelector('.card-name');
+const numberEl = document.querySelector('.card-number');
+const dateEl = document.querySelector('.card-date');
+const cvcEl = document.querySelector('.card-cvc');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  nameEl.innerHTML = nameInput.value.toUpperCase();
+  numberEl.innerHTML = format(cardNumberInput.value);
+  dateEl.innerHTML = monthInput.value + '/' + yearInput.value;
+  cvcEl.innerHTML = cvcInput.value;
+});
+
+function format(num) {
+  return num.toString().replace(/\d{4}(?=.)/g, '$& ');
+}
